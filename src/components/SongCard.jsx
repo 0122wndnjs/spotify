52655015fb066e0dd49cbd73625896ac6d30537c
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-11-23 03:57:50 +0900

    play pause function works

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -7,9 +7,16 @@ import { playPause, setActiveSong } from "../redux/features/playerSlice";
 const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
   // const activeSong = "Test";
 
-  const handlePauseClick = () => {};
+  const dispatch = useDispatch();
 
-  const handlePlayClick = () => {};
+  const handlePauseClick = () => {
+    dispatch(playPause(false))
+  };
+
+  const handlePlayClick = () => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  };
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
