8314d864fcbdd5a2b3c0b1ce4ec2fb362fbab97b
Joowon Kim <32347532+0122wndnjs@users.noreply.github.com>
2022-11-23 03:54:31 +0900

    adding play pause icon

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,8 +4,12 @@ import { useDispatch } from "react-redux";
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
-const SongCard = ({ song, i }) => {
-  const activeSong = "Test";
+const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
+  // const activeSong = "Test";
+
+  const handlePauseClick = () => {};
+
+  const handlePlayClick = () => {};
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
@@ -17,7 +21,13 @@ const SongCard = ({ song, i }) => {
               : "hidden"
           }`}
         >
-          <PlayPause />
+          <PlayPause
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+          />
         </div>
         <img alt="song_img" src={song.images?.coverart} />
       </div>
@@ -26,7 +36,15 @@ const SongCard = ({ song, i }) => {
           <Link to={`/songs/${song?.key}`}>{song.title}</Link>
         </p>
         <p className="text-sm truncate text-gray-300 mt-1">
-          <Link to={song.artists ? `/artists/${song?.artists[0]?.adamid}` : "top-artists"}>{song.subtitle}</Link>
+          <Link
+            to={
+              song.artists
+                ? `/artists/${song?.artists[0]?.adamid}`
+                : "top-artists"
+            }
+          >
+            {song.subtitle}
+          </Link>
         </p>
       </div>
     </div>
